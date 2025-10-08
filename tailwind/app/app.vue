338e117c187943b48791a2289320a11b248172fe
Leogeox <leovginx@gmail.com>
2025-10-08 13:01:16 +0200

    Creation nav

--- tailwind/app/app.vue
+++ tailwind/app/app.vue
@@ -1,7 +1,8 @@
 <template>
-  <main class="flex bg-white-900">
-    <div class="flex-col max-w-84 h-screen bg-red-500">
-      <div class="flex justify-center pt-6 text-xl ">TAILWIND</div>
-    </div>
+  <main class="flex min-h-screen">
+      <Nav/>
+      <div>
+        <Header/>
+      </div>
   </main>
 </template>
---
+++ tailwind/app/components/nav.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="min-w-80 bg-lightgrey h-screen">
+        <div class="flex justify-center pt-6 text-2xl">TAILWIND</div>
+    </div>
+</template>
