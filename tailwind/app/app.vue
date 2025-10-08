37958b16e0d65e89e9448d00668c42c3db61cbd2
Leogeox <leovginx@gmail.com>
2025-10-08 12:35:19 +0200

    configuration de nuxt et tailwind

--- tailwind/app/app.vue
+++ tailwind/app/app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
-    cc
+  <div class="flex flex-col max-w-64 h-screen bg-red-500">
+    <div class="flex justify-center pt-6 text-xl ">TAILWIND</div>
   </div>
 </template>
---
+++ tailwind/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  colors: {
+    black: rgba(0, 0, 0, 1),
+    lightgrey: rgba(238, 241, 255, 1),
+    grey: rgba(129, 130, 140, 1),
+    white: rgba(255, 255, 255, 1),
+    blue: rgba(70, 58, 221, 1),
+    violet: rgba(255, 0, 251, 1)
+  },
+}
+
