4e795898b101c13af4b58e765f7ab9abb28eb526
Leogeox <leovginx@gmail.com>
2025-10-10 17:20:25 +0200

    enlevage elements non necessaire

--- tailwind/app/app.vue
+++ tailwind/app/app.vue
@@ -14,7 +14,7 @@
             <p class="text-grey mt-4 font-regular">Create new catalog to add products and generate content.</p>
           </div> 
 
-          <Button class="justify-center gap-1 mt-12 sm:p-4 md:p-4 lg:p-2"/>
+          <Button class="justify-center gap-1 mt-12 p-4 lg:p-2"/>
         </Post>
 
         <!-- Post numero 2 -->
--- tailwind/app/components/post.vue
+++ tailwind/app/components/post.vue
@@ -1,5 +1,5 @@
 <template>
-    <li class="flex flex-col w-full h-auto mt-10 border-2 border-border rounded-lg p-3 list-none max-sm:mt-1 sm:mt-3 md:mt-3 xl:m-4">
+    <li class="flex flex-col w-full h-auto mt-10 border-2 border-border rounded-lg p-3 list-none max-sm:mt-1 sm:mt-3 xl:m-4">
         <slot/>
     </li>
 </template>
